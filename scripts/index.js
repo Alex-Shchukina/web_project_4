7d0332f18af4dbe42ecdfe6d324f250247b74277
Aleksandra Shchukina <alexshchukina90@gmail.com>
2022-01-03 16:47:09 +0200

    Finish JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,39 @@
 let popupBoxCloseButton = document.querySelector(".popup__close-button");
 let popupBox = document.querySelector(".popup");
 let popupOpenButton = document.querySelector(".profile__edit-button");
+let userNameInfo = document.querySelector(".profile__name");
+let userOccupationInfo = document.querySelector(".profile__occupation");
+let inputName = document.querySelector(".popup__name");
+let inputOccupation = document.querySelector(".popup__about");
+let formElement = document.querySelector(".popup__form");
 
-function togglePopupBox() {
-  popupBox.classList.toggle("popup_opened");
+function openPopupBox() {
+  let userName = userNameInfo.textContent;
+  let userOccupation = userOccupationInfo.textContent;
+
+  inputName.value = userName;
+  inputOccupation.value = userOccupation;
+
+  popupBox.classList.add("popup_opened");
 }
 
-popupOpenButton.addEventListener("click", togglePopupBox);
-popupBoxCloseButton.addEventListener("click", togglePopupBox);
+function closePopupBox() {
+  popupBox.classList.remove("popup_opened");
+}
+
+popupOpenButton.addEventListener("click", openPopupBox);
+popupBoxCloseButton.addEventListener("click", closePopupBox);
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let inputNameValue = inputName.value;
+  let inputOccupationValue = inputOccupation.value;
+
+  userNameInfo.textContent = inputNameValue;
+  userOccupationInfo.textContent = inputOccupationValue;
+
+  closePopupBox();
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
